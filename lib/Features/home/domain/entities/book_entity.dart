0451cf102fc7a31564bc2df4542ee808d620687e
mohamed taha <mt164371@gmail.com>
2026-09-08 14:13:49 +0300

    create type adapter for the book entity

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,17 +1,28 @@
+import 'package:hive/hive.dart';
+
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String entityId;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String? image;
+  @HiveField(3)
   final String? authorName;
+  @HiveField(4)
   final num? price;
+  @HiveField(5)
   final num? rating;
 
-  BookEntity(
-   { required this.title,
+  BookEntity({
+    required this.title,
     required this.image,
     required this.authorName,
     required this.price,
     required this.rating,
-    required this.entityId,}
-  );
+    required this.entityId,
+  });
 }
