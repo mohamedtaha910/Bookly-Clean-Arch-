ad656dbaa9499ec3d5dc621fe699c4aec22b5a33
mohamed taha <mt164371@gmail.com>
2026-09-09 13:46:37 +0300

    create server failure

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,4 +1,4 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
+// import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
 
 import 'package:bookly/Features/home/data/data_sources/home_local_data_source.dart';
 import 'package:bookly/Features/home/data/data_sources/home_remote_data_source.dart';
@@ -6,6 +6,7 @@ import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImplementation extends HomeRepo {
   final HomeRemoteDataSource homeRemoteDataSource;
@@ -27,7 +28,10 @@ class HomeRepoImplementation extends HomeRepo {
       books = await homeRemoteDataSource.fetchFeaturedBooks();
       return right(books);
     } on Exception catch (e) {
-      return left(Failure(errorMessage: e.toString()));
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(errorMessage: e.toString()));
     }
   }
 
@@ -43,7 +47,10 @@ class HomeRepoImplementation extends HomeRepo {
       books = await homeRemoteDataSource.fetchNewestBooks();
       return right(books);
     } on Exception catch (e) {
-      return left(Failure(errorMessage: e.toString()));
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(errorMessage: e.toString()));
     }
   }
 }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,6 +1,59 @@
-class Failure {
+import 'package:dio/dio.dart';
+
+abstract class Failure {
   final String errorMessage;
 
   Failure({required this.errorMessage});
+}
 
+class ServerFailure extends Failure {
+  ServerFailure({required super.errorMessage});
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(errorMessage: "Connection timeout with ApiServer");
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(errorMessage: "Send timeout with ApiServer");
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(errorMessage: "Receive timeout with ApiServer");
+      case DioExceptionType.badCertificate:
+        return ServerFailure(errorMessage: "Bad certificate with ApiServer");
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+          dioError.response?.statusCode ?? 0,
+          dioError.response?.data,
+        );
+      case DioExceptionType.cancel:
+        return ServerFailure(errorMessage: "Request canceled with ApiServer");
+      case DioExceptionType.connectionError:
+        return ServerFailure(errorMessage: "No internet connection");
+      case DioExceptionType.unknown:
+        return ServerFailure(
+          errorMessage: "Unexpected error , please try again!",
+        );
+      case DioExceptionType.transformTimeout:
+        return ServerFailure(errorMessage: "Transform timeout with ApiServer");
+    }
+  }
+
+  /// Creates a [ServerFailure] instance based on the HTTP status code and response data.
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(errorMessage: response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure(
+        errorMessage: 'Your request not found, Please try later',
+      );
+    } else if (statusCode == 500) {
+      return ServerFailure(
+        errorMessage: 'Internal Server error, Please try later',
+      );
+    } else if (statusCode == 429) {
+      return ServerFailure(errorMessage: 'Too many requests, Please try later');
+    } else {
+      return ServerFailure(
+        errorMessage: 'Opps there was an error, Please try later',
+      );
+    }
+  }
 }
